0c2d285982a4dcbd2b4303f79e73745515754926
Pepe Coral <coritochico@gmail.com>
2025-09-15 19:29:47 +0200

    fix: :bug: fixing missing files in spring project

---
+++ backend/src/main/java/com/pepe/foodforge/FoodforgeApplication.java
@@ -0,0 +1,13 @@
+package com.pepe.foodforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodforgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodforgeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/pepe/foodforge/FoodforgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pepe.foodforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodforgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
